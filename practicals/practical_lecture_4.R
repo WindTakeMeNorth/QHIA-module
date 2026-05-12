93a8f683489a087e604e45e0a18f0c186657b2ab
Yihan Hu <huyihan2022@gmail.com>
2026-05-12 10:36:41 +0100

    1

--- practicals/practical_lecture_4.R
+++ practicals/practical_lecture_4.R
@@ -39,19 +39,20 @@ incidence <- 0.06 #60 per 1000 IHD death per year
 ##### 3. Convert exposure to RR ####
 
 ##### What is the RR at baseline for each person
-df$RR_baseline # <-
-
+                        
+df$RR_baseline  <- 1.1^( (df$pm25_baseline/increment))
 ##### What is the RR in the counterfactual scenario for each person
-df$RR_scenario # <-
+df$RR_scenario  <- 1.1^( (df$pm25_scenario/increment))
 
 
 ##### 4. Compute PIF
 
-##### What is the PIF
-
+PIF <- mean(df$RR_baseline - df$RR_scenario)/mean(df$RR_baseline)
+PIF
 
 ##### 5. Calculate cases
 cases_baseline <- incidence*nrow(df)
 cases_prevented <- cases_baseline*PIF
 
 paste0("The PIF is ", round(PIF,2), " so the cases prevented are ", round(cases_prevented, 2))
+
